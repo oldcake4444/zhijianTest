eba7b961b217e56a5b089d6dfe087b51d576293a
oldcake4444 <oldcake4444@163.com>
2023-12-13 19:30:20 +0800

    update test cases

--- src/test/java/com/test/InterfaceTest/CucumberRunEntryPoint.java
+++ src/test/java/com/test/InterfaceTest/CucumberRunEntryPoint.java
@@ -10,8 +10,8 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions (
 		//plugin = {"pretty", "html:src/main/resources/CucumberReports/html", "json:src/main/resources/CucumberReports/json/cucumber.json", "junit:target/junit"}, 
 		plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber.json", "rerun:target/rerun.txt"},
-		//features = {"src/main/resources/Features/Interface/AppApi_ZJLogin.feature","src/main/resources/Features/Interface/AppApi_UserRelatedOrgInfo.feature"},
-		features = {"src/main/resources/Features/Interface/AppApi_UserRelatedOrgInfo.feature"},
+		features = {"src/main/resources/Features/Interface/AppApi_ZJLogin.feature","src/main/resources/Features/Interface/AppApi_UserRelatedOrgInfo.feature"},
+		//features = {"src/main/resources/Features/Interface/AppApi_UserRelatedOrgInfo.feature"},
 		glue = {"com.test"}
 		//tags = {"@AppApiTest"}
 		)
